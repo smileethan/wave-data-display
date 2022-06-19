a8ae223429833a3e67b8101cebcb95e8da441890
Ethan Zhou <smile_ethan@126.com>
2022-06-19 20:23:41 +0800

    Init
    Init

---
+++ Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace WaveDataDisplay
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.pbWave = new System.Windows.Forms.PictureBox();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.btnZoomIn = new System.Windows.Forms.Button();
+            this.btnZoomOut = new System.Windows.Forms.Button();
+            this.cbHex = new System.Windows.Forms.CheckBox();
+            this.btnExportImg = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbWave)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(12, 12);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenFile.TabIndex = 0;
+            this.btnOpenFile.Text = "打开文件";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenfile_Click);
+            // 
+            // pbWave
+            // 
+            this.pbWave.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbWave.Location = new System.Drawing.Point(0, 0);
+            this.pbWave.Name = "pbWave";
+            this.pbWave.Size = new System.Drawing.Size(828, 571);
+            this.pbWave.TabIndex = 1;
+            this.pbWave.TabStop = false;
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Location = new System.Drawing.Point(94, 11);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(75, 23);
+            this.btnLeft.TabIndex = 2;
+            this.btnLeft.Text = "←";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            this.btnLeft.Click += new System.EventHandler(this.btnLeft_Click);
+            // 
+            // btnRight
+            // 
+            this.btnRight.Location = new System.Drawing.Point(175, 11);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(75, 23);
+            this.btnRight.TabIndex = 3;
+            this.btnRight.Text = "→";
+            this.btnRight.UseVisualStyleBackColor = true;
+            this.btnRight.Click += new System.EventHandler(this.btnRight_Click);
+            // 
+            // btnUp
+            // 
+            this.btnUp.Location = new System.Drawing.Point(256, 11);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(75, 23);
+            this.btnUp.TabIndex = 4;
+            this.btnUp.Text = "↑";
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
+            // 
+            // btnDown
+            // 
+            this.btnDown.Location = new System.Drawing.Point(337, 11);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(75, 23);
+            this.btnDown.TabIndex = 5;
+            this.btnDown.Text = "↓";
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+            // 
+            // btnZoomIn
+            // 
+            this.btnZoomIn.Location = new System.Drawing.Point(418, 11);
+            this.btnZoomIn.Name = "btnZoomIn";
+            this.btnZoomIn.Size = new System.Drawing.Size(75, 23);
+            this.btnZoomIn.TabIndex = 6;
+            this.btnZoomIn.Text = "+";
+            this.btnZoomIn.UseVisualStyleBackColor = true;
+            this.btnZoomIn.Click += new System.EventHandler(this.btnZoomIn_Click);
+            // 
+            // btnZoomOut
+            // 
+            this.btnZoomOut.Location = new System.Drawing.Point(499, 11);
+            this.btnZoomOut.Name = "btnZoomOut";
+            this.btnZoomOut.Size = new System.Drawing.Size(75, 23);
+            this.btnZoomOut.TabIndex = 7;
+            this.btnZoomOut.Text = "-";
+            this.btnZoomOut.UseVisualStyleBackColor = true;
+            this.btnZoomOut.Click += new System.EventHandler(this.btnZoomOut_Click);
+            // 
+            // cbHex
+            // 
+            this.cbHex.AutoSize = true;
+            this.cbHex.Location = new System.Drawing.Point(581, 13);
+            this.cbHex.Name = "cbHex";
+            this.cbHex.Size = new System.Drawing.Size(72, 16);
+            this.cbHex.TabIndex = 8;
+            this.cbHex.Text = "十六进制";
+            this.cbHex.UseVisualStyleBackColor = true;
+            // 
+            // btnExportImg
+            // 
+            this.btnExportImg.Location = new System.Drawing.Point(660, 13);
+            this.btnExportImg.Name = "btnExportImg";
+            this.btnExportImg.Size = new System.Drawing.Size(75, 23);
+            this.btnExportImg.TabIndex = 9;
+            this.btnExportImg.Text = "导出图片";
+            this.btnExportImg.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(828, 571);
+            this.Controls.Add(this.btnExportImg);
+            this.Controls.Add(this.cbHex);
+            this.Controls.Add(this.btnZoomOut);
+            this.Controls.Add(this.btnZoomIn);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnUp);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.btnLeft);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.pbWave);
+            this.Name = "MainForm";
+            this.Text = "波形数据显示";
+            this.Shown += new System.EventHandler(this.MainForm_Shown);
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pbWave)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.PictureBox pbWave;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Button btnZoomIn;
+        private System.Windows.Forms.Button btnZoomOut;
+        private System.Windows.Forms.CheckBox cbHex;
+        private System.Windows.Forms.Button btnExportImg;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+using System.IO;
+
+namespace WaveDataDisplay
+{
+    public partial class MainForm : Form
+    {
+        private ArrayList m_arrData = new ArrayList(); // 数据列表
+        private float m_fDataMax; // 最大值
+        private float m_fDataMin; // 最小值
+        Bitmap m_bmpWave; // 显示位图
+
+        float m_fDataRangeX = 500.0f; // x轴 数据范围
+        int m_nMarkCntX = 10; // x轴 刻度数量
+        PointF m_ptDataOffset = new PointF(100.0f, 100.0f); // 数据偏移
+        float m_fMargin = 30.0f; // 留白
+        float m_fScaleDTV = 1.0f;
+        float m_fMarkSpan = 1.0f;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        
+        private void btnOpenfile_Click(object sender, EventArgs e)  // 加载文件按钮
+        {
+            m_fDataMax = float.MinValue;
+            m_fDataMin = float.MaxValue;
+            m_arrData = new ArrayList();
+
+            // 读取文件
+            string strFileData = "";
+            try
+            {
+                OpenFileDialog dlgOpen = new OpenFileDialog();
+                dlgOpen.ShowDialog();
+                StreamReader srReader = new StreamReader(dlgOpen.FileName);
+                strFileData = srReader.ReadToEnd();
+                srReader.Close();
+            }
+            catch
+            {
+                MessageBox.Show("文件读取失败！", "错误");
+                return;
+            }
+
+            // 数据解析
+            string[] arrFileDataSplit = strFileData.Split();
+            for(int i=0; i< arrFileDataSplit.Length; i++)
+            {
+                string strData = arrFileDataSplit[i].Trim();
+                if (strData.Length == 0)
+                    continue;
+                float fData;
+                try
+                {
+                    fData = float.Parse(strData);
+                }
+                catch
+                {
+                    MessageBox.Show(string.Format("解析第{0:D}个数据【{1:S}】错误，已忽略", m_arrData.Count + 1, strData));
+                    continue;
+                }
+                m_arrData.Add(fData);
+                if (fData > m_fDataMax) m_fDataMax = fData;
+                if (fData < m_fDataMin) m_fDataMin = fData;
+            }
+            // 刷新界面
+            Display();
+            return;
+        }
+
+        private PointF CoordTrans_DataToView(int nViewWidth, int nViewHeigh, float fScaleDTV, // 坐标转换 从数据到视图
+            PointF ptDataOffset, PointF ptViewOffset, PointF ptDataPt)
+        {
+            return new PointF(
+                ptViewOffset.X + (nViewWidth) * 0.5f + (ptDataPt.X - ptDataOffset.X) * fScaleDTV,
+                ptViewOffset.Y + (nViewHeigh) * 0.5f - (ptDataPt.Y - ptDataOffset.Y) * fScaleDTV
+            );
+
+        }
+        private bool CoordTrans_DataToView(int nViewWidth, int nViewHeigh, float fScaleDTV, // 批量坐标转换 从数据到视图
+            PointF ptDataOffset, PointF ptViewOffset, ref PointF[] arrDataPts)
+        {
+            for (int i = 0; i < arrDataPts.Length; i++)
+                arrDataPts[i] = CoordTrans_DataToView(nViewWidth, nViewHeigh, fScaleDTV, 
+                    ptDataOffset, ptViewOffset, arrDataPts[i]);
+            return true;
+        }
+
+        private void Display() // 刷新界面
+        {
+            // 视图范围
+            int nViewWidth = pbWave.Width;  
+            int nViewHeight = pbWave.Height;
+            // 比例系数 从数据到视图 
+            m_fScaleDTV = (nViewWidth - m_fMargin * 2) / m_fDataRangeX;  
+
+            // 数据坐标范围
+            float fDataRangeXMin = -m_fDataRangeX / 2.0f + m_ptDataOffset.X;
+            float fDataRangeXMax = m_fDataRangeX / 2.0f + m_ptDataOffset.X;
+            float fDataRangeYMin = -(nViewHeight * 0.5f - m_fMargin) / m_fScaleDTV + m_ptDataOffset.Y;
+            float fDataRangeYMax = (nViewHeight * 0.5f - m_fMargin) / m_fScaleDTV + m_ptDataOffset.Y;
+
+            // 创建画布
+            m_bmpWave = new Bitmap(nViewWidth, nViewHeight);
+            Graphics gWave = Graphics.FromImage(m_bmpWave);
+            Pen penBlkThin = new Pen(Color.Black, 1);
+            Pen penRedMid = new Pen(Color.Red, 2);
+            Font fontBlkThin = new Font("宋体", 12);
+            SolidBrush brshBlk = new SolidBrush(Color.Black);
+            // 绘制坐标轴
+            // 绘制直线
+            PointF[] arrPts = new PointF[2];
+            arrPts[0] = CoordTrans_DataToView(nViewWidth, nViewHeight, m_fScaleDTV, m_ptDataOffset, new PointF(-m_fMargin, 0.0f), new PointF(fDataRangeXMin, 0.0f));
+            arrPts[1] = CoordTrans_DataToView(nViewWidth, nViewHeight, m_fScaleDTV, m_ptDataOffset, new PointF(m_fMargin, 0.0f), new PointF(fDataRangeXMax, 0.0f));
+            gWave.DrawLine(penBlkThin, arrPts[0].X, arrPts[0].Y, arrPts[1].X, arrPts[1].Y);
+            arrPts[0] = CoordTrans_DataToView(nViewWidth, nViewHeight, m_fScaleDTV, m_ptDataOffset, new PointF(0.0f, m_fMargin), new PointF(0.0f, fDataRangeYMin));
+            arrPts[1] = CoordTrans_DataToView(nViewWidth, nViewHeight, m_fScaleDTV, m_ptDataOffset, new PointF(0.0f, -m_fMargin), new PointF(0.0f, fDataRangeYMax));
+            gWave.DrawLine(penBlkThin, arrPts[0].X, arrPts[0].Y, arrPts[1].X, arrPts[1].Y);
+            // 绘制箭头
+            arrPts = new PointF[3]; 
+            arrPts[0] = CoordTrans_DataToView(nViewWidth, nViewHeight, m_fScaleDTV, m_ptDataOffset, new PointF(m_fMargin-10.0f, -5.0f), new PointF(fDataRangeXMax, 0.0f));
+            arrPts[1] = CoordTrans_DataToView(nViewWidth, nViewHeight, m_fScaleDTV, m_ptDataOffset, new PointF(m_fMargin, 0.0f), new PointF(fDataRangeXMax, 0.0f));
+            arrPts[2] = CoordTrans_DataToView(nViewWidth, nViewHeight, m_fScaleDTV, m_ptDataOffset, new PointF(m_fMargin-10.0f, +5.0f), new PointF(fDataRangeXMax, 0.0f));
+            gWave.DrawLines(penBlkThin, arrPts);
+            arrPts[0] = CoordTrans_DataToView(nViewWidth, nViewHeight, m_fScaleDTV, m_ptDataOffset, new PointF(-5.0f, 10.0f - m_fMargin), new PointF(0.0f, fDataRangeYMax));
+            arrPts[1] = CoordTrans_DataToView(nViewWidth, nViewHeight, m_fScaleDTV, m_ptDataOffset, new PointF(0.0f, -m_fMargin), new PointF(0.0f, fDataRangeYMax));
+            arrPts[2] = CoordTrans_DataToView(nViewWidth, nViewHeight, m_fScaleDTV, m_ptDataOffset, new PointF(5.0f, 10.0f - m_fMargin), new PointF(0.0f, fDataRangeYMax));
+            gWave.DrawLines(penBlkThin, arrPts);
+            // 绘制原点
+            arrPts[0] = CoordTrans_DataToView(nViewWidth, nViewHeight, m_fScaleDTV, m_ptDataOffset, new PointF(-25.0f, 0.0f), new PointF(0.0f, 0.0f)); ;
+            gWave.DrawString("O", fontBlkThin, brshBlk, arrPts[0]);
+            // 绘制刻度
+            arrPts = new PointF[2];
+            m_fMarkSpan = (fDataRangeXMax - fDataRangeXMin) / m_nMarkCntX;
+            for (float fMark = fDataRangeXMin; fMark <= fDataRangeXMax; fMark += m_fMarkSpan)
+            {
+                if (fMark == 0)
+                    continue;
+                arrPts[0] = CoordTrans_DataToView(nViewWidth, nViewHeight, m_fScaleDTV, m_ptDataOffset, new PointF(0.0f, -5.0f), new PointF(fMark, 0.0f));
+                arrPts[1] = CoordTrans_DataToView(nViewWidth, nViewHeight, m_fScaleDTV, m_ptDataOffset, new PointF(0.0f, 5.0f), new PointF(fMark, 0.0f));
+                gWave.DrawLine(penBlkThin, arrPts[0], arrPts[1]);
+                arrPts[1].X -= 20.0f;
+                gWave.DrawString(string.Format("{0:F1}", fMark), fontBlkThin, brshBlk, arrPts[1]);
+            }
+            for (float fMark = fDataRangeYMin; fMark <= fDataRangeYMax; fMark += m_fMarkSpan)
+            {
+                if (fMark == 0)
+                    continue;
+                arrPts[0] = CoordTrans_DataToView(nViewWidth, nViewHeight, m_fScaleDTV, m_ptDataOffset, new PointF(-5.0f, 0.0f), new PointF(0.0f, fMark));
+                arrPts[1] = CoordTrans_DataToView(nViewWidth, nViewHeight, m_fScaleDTV, m_ptDataOffset, new PointF(5.0f, 0.0f), new PointF(0.0f, fMark));
+                gWave.DrawLine(penBlkThin, arrPts[0], arrPts[1]);
+                arrPts[1].X -= 50.0f;
+                arrPts[1].Y -= 10.0f;
+                gWave.DrawString(string.Format("{0:F1}",fMark), fontBlkThin, brshBlk, arrPts[1]);
+            }
+
+            // 绘制数据
+            if(m_arrData.Count > 1)
+            {
+                PointF[] arrPtData = new PointF[m_arrData.Count];
+                for (int i = 0; i < m_arrData.Count; i++)
+                {
+                    arrPtData[i] = new PointF(i, (float)m_arrData[i]);
+                }
+                CoordTrans_DataToView(nViewWidth, nViewHeight, m_fScaleDTV, m_ptDataOffset, new PointF(0.0f, 0.0f), ref arrPtData);
+                //gWave.DrawLines(penRedMid, arrPtData);
+                gWave.DrawCurve(penRedMid, arrPtData);
+
+            }
+
+            // 显示
+            pbWave.Image = m_bmpWave;
+            // 释放资源
+            penRedMid.Dispose();
+            penBlkThin.Dispose();
+            fontBlkThin.Dispose();
+            brshBlk.Dispose();
+            gWave.Dispose();
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            Display();
+        }
+
+        private void btnLeft_Click(object sender, EventArgs e)
+        {
+            m_ptDataOffset.X -= m_fMarkSpan;
+            Display();
+        }
+
+        private void btnRight_Click(object sender, EventArgs e)
+        {
+            m_ptDataOffset.X += m_fMarkSpan;
+            Display();
+
+        }
+
+        private void btnUp_Click(object sender, EventArgs e)
+        {
+            m_ptDataOffset.Y += m_fMarkSpan;
+            Display();
+
+        }
+
+        private void btnDown_Click(object sender, EventArgs e)
+        {
+            m_ptDataOffset.Y -= m_fMarkSpan;
+            Display();
+
+        }
+
+        private void btnZoomIn_Click(object sender, EventArgs e)
+        {
+            m_fDataRangeX *= 0.5f;
+            Display();
+        }
+
+        private void btnZoomOut_Click(object sender, EventArgs e)
+        {
+            m_fDataRangeX *= 2.0f;
+            Display();
+        }
+
+        private void MainForm_Shown(object sender, EventArgs e)
+        {
+            Display();
+        }
+    }
+}
